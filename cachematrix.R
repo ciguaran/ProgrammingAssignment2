9af7eed8d1f3e86baaf3d371032bc1f4ffc56dde
Carlos Iguaran <ciiguaran@gmail.com>
2021-06-29 14:01:15 -0300

    resolution

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_precomputed <- NULL
+  set_inverse = function() inverse_precomputed <<- solve(x)
+  get_inverse = function() inverse_precomputed
+  list(matrix = x, 
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
@@ -12,4 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get_inverse()
+  if(is.null(inverse)) {
+    x$set_inverse()
+  }
+  x$get_inverse()
 }
